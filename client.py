5a0ae434ae30b1b75a3493f24ef907c499f3114c
luccamansilla <luccamansilla01@gmail.com>
2024-08-30 17:20:49 -0300

    nombres, validación si tiene y gitignore

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import serpent
+import os
 from Pyro5.api import Proxy
 
 print("Enter the server's uri that was printed:")
@@ -6,13 +7,14 @@ uri = input().strip()
 
 with open("romeo.mp3", "rb") as file:
     data = file.read()
+    filename = os.path.basename(file.name)
 
 datasize = len(data)
 
 def do_test(data, datasize):
     with Proxy(uri) as obj:
         obj._pyroBind()
-        obj.transfer(data)
-        print("¡Archivo transferido!!")
+        obj.transfer(data, filename)
+        print("¡Archivo transferido!")
 
 do_test(data, datasize)
--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import Pyro5.socketutil
 import os
 import hashlib
 
-save_directory = "C:/Users/lucca/Desktop/py/DISTIBUIDOS/ArchivoGuardado"
+save_directory = "C:/Users/lucca/Desktop/py/Jam-SD/songs"
 
 # Verifica que la carpeta existe y muestra un mensaje
 if not os.path.exists(save_directory):
@@ -15,20 +15,22 @@ else:
 
 class Testclass(object):
     @expose
-    def transfer(self, data):
+    def transfer(self, data, filename):
         if config.SERIALIZER == "serpent" and isinstance(data, dict):
             data = serpent.tobytes(data)  # Convertir el diccionario en bytes si es necesario
         
-        file_path = os.path.join(save_directory, "tema_recibido.mp3")
+        file_path = os.path.join(save_directory, filename)
         print(f"Ruta de archivo configurada: {file_path}")
 
-        try:
-            with open(file_path, "wb") as f:
-                f.write(data)
-                print(f"Archivo guardado en: {file_path}")
-        except Exception as e:
-            print(f"Failed to save the file: {e}")
-        
+        if not os.path.exists(file_path):
+            try:
+                with open(file_path, "wb") as f:
+                    f.write(data)
+                    print(f"Archivo guardado en: {file_path}")
+            except Exception as e:
+                print(f"Failed to save the file: {e}")
+        else:
+            print("Ya existe un archivo con el mismo nombre, no se volvio a guardar el archivo")
         return len(data)
 
 serve(
